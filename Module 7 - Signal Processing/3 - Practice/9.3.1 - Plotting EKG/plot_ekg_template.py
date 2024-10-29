2c0560af6dd21f820fe6e7fa7594bb22ebd349eb
E-alt-F4 <sheehyem@dukes.jmu.edu>
2024-10-29 12:01:56 -0400

    Jackson Werner

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -3,16 +3,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # import the CSV file using numpy
+
 path = '../../../data/ekg/mitdb_201.csv'
-
 # load data in matrix from CSV file; skip first two rows
-
-### Your code here ###
+data= np.loadtxt(path,delimiter=',',skiprows=2)
 
 # save each vector as own variable
+First = []
+Second =[]
+Third =[]
 
-### Your code here ###
+First = data[:,0]
+Second = data[:,1]
+Third = data[:,2]
 
 # use matplot lib to generate a single
-
-### Your code here ###
+plt.plot(First,Second)
+plt.plot(First,Third)
+plt.title("MLII & V1 over Time")
+plt.xlabel("Time")
+plt.ylabel("MLII & V1")
+plt.show()
--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy
+import math
 
 """
 Step 0: Select which database you wish to use.
@@ -20,31 +22,60 @@ Step #1: load data in matrix from CSV file; skip first two rows. Call the data s
 
 signal = 0
 ## YOUR CODE HERE ##
+signal = np.loadtxt(signal_filepath,delimiter=',',skiprows=2)
+plt.title('Process Signal for ' + database_name)
+plt.plot(signal)
+plt.show()
 
+Time = signal[:,0]
+ML2 = signal[:,1]
+V1 = signal[:,2]
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
 """
 
 ## YOUR CODE HERE ##
-
+ ## filtered_signal = scipy.signal.butter()
 """
 Step 3: Pass data through weighted differentiator
 """
-
 ## YOUR CODE HERE ##
+mag_Time = np.sqrt(Time**2)
+mag_ML2 = np.sqrt(ML2**2)
+mag_V1 = np.sqrt(V1**2)
+diff_Time = np.diff(mag_Time)
+diff_ML2 = np.diff(mag_ML2)
+diff_V1 = np.diff(mag_V1)
 
-
+plt.title('Difference Signal')
+plt.plot(diff_V1)
+plt.xlabel('Time')
+plt.ylabel('Difference of V1')
+plt.show()
 """
 Step 4: Square the results of the previous step
 """
  ## YOUR CODE HERE ##
+square_Time = pow(diff_Time,2)
+square_ML2 = pow(diff_ML2,2)
+square_V1 = pow(diff_V1,2)
 
+plt.title('Squared signal')
+plt.plot(square_V1)
+plt.xlabel('Time')
+plt.ylabel('Square of V1')
+plt.show()
 """
 Step 5: Pass a moving filter over your data
 """
-
 ## YOUR CODE HERE
-# make a plot of the results. Can change the plot() parameter below to show different intermediate signals
-plt.title('Process Signal for ' + database_name)
-plt.plot(signal)
+comparison = np.ones(5)
+moving_average_ML2 = np.convolve(square_ML2,comparison)
+moving_average_V1 = np.convolve(square_V1, comparison)
+
+plt.title('Moving average of signal')
+plt.plot(moving_average_V1)
+plt.xlabel('Time')
+plt.ylabel('Moving average of V1')
 plt.show()
+# make a plot of the results. Can change the plot() parameter below to show different intermediate signals
